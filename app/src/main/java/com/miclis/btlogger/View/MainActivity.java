7f7831679c32423ca9dbd823d6609928f01e3f86
Michał Lisowski <michael.lisowski@zoho.eu>
2018-11-19 22:03:29 +0100

    Initial commit

---
+++ app/src/main/java/com/miclis/btlogger/View/MainActivity.java
@@ -0,0 +1,14 @@
+package com.miclis.btlogger.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.miclis.btlogger.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
